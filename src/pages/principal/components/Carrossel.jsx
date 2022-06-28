9976418a447c1dfacdb7fc34034cb2e89452a76f
GABRIEL BAUNILIA SILVA <gabriel.bsilva80@senacsp.edu.br>
2022-06-27 22:09:28 -0300

    Salvo

--- src/pages/principal/components/Carrossel.jsx
+++ src/pages/principal/components/Carrossel.jsx
@@ -2,54 +2,17 @@ import { Button, Card, Carousel, Col, Container, Row } from "react-bootstrap";
 
 export default function Carrosel() {
     return (
-        <Carousel className="w-50">
-            <Carousel.Item>
-                <Card class="position-absolute top-0 start-100">
-                    <Card.Img variant="top" src="holder.js/100px180" />
-                    <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
-                <Card>
-                    <Card.Img variant="top" src="holder.js/100px180" />
-                    <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
+        <Carousel className="w-25">
+            <Carousel.Item class="d-inline">
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="https://listenx.com.br/blog/wp-content/uploads/2020/06/layout-de-loja.jpg" />
                 </Card>
             </Carousel.Item>
             <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src="holder.js/800x400?text=Second slide&bg=282c34"
-                    alt="Second slide"
-                />
 
-                <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src="holder.js/800x400?text=Third slide&bg=20232a"
-                    alt="Third slide"
-                />
 
-                <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                </Carousel.Caption>
             </Carousel.Item>
         </Carousel>
     )
