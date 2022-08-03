90d121120dc412b1ab19b790307fe02381ccd8f9
GABRIEL BAUNILIA SILVA <gabriel.bsilva80@senacsp.edu.br>
2022-08-03 20:25:29 -0300

    .

--- src/pages/principal/components/Carrossel.jsx
+++ src/pages/principal/components/Carrossel.jsx
@@ -2,17 +2,21 @@ import { Button, Card, Carousel, Col, Container, Row } from "react-bootstrap";
 
 export default function Carrosel() {
     return (
-        <Carousel className="w-25">
+        <Carousel className="w-50">
             <Carousel.Item class="d-inline">
                 <Card style={{ width: '18rem' }}>
                     <Card.Img variant="top" src="https://listenx.com.br/blog/wp-content/uploads/2020/06/layout-de-loja.jpg" />
                 </Card>
             </Carousel.Item>
             <Carousel.Item>
-
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="https://listenx.com.br/blog/wp-content/uploads/2020/06/layout-de-loja.jpg" />
+                </Card>
             </Carousel.Item>
             <Carousel.Item>
-
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="https://listenx.com.br/blog/wp-content/uploads/2020/06/layout-de-loja.jpg" />
+                </Card>
             </Carousel.Item>
         </Carousel>
     )
